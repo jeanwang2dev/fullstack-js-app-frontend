e2f3bff063131b6bb0a41569ece3d78ad404a484
jeanwang2dev <jeanharmsen@yahoo.com>
2021-11-05 17:55:09 -0400

    add Header and Button componets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="container">
+    <Header title="Tweets Tacker" />
     <!-- create a tweet -->
     <form @submit.prevent="createTweet">
       <input type="text" v-model="text"/>
@@ -13,8 +14,13 @@
 </template>
 
 <script>
+import Header from './components/Header'
+
 export default {
   name: 'App',
+  components: {
+    Header
+  },
   data() {
     return {
       text: '',
@@ -45,12 +51,45 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="onClick()" 
+            :style="{ background: color }" 
+            class="btn">{{ btnText }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        btnText: String,
+        color: String
+    },
+    methods: {
+        onClick(){
+            console.log("Btn clicked!")
+        }
+    }
+}
+</script>
