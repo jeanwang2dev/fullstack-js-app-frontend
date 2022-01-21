aa04be34f073d90840fb16268111bfd3452a3923
jeanwang2dev <jeanharmsen@yahoo.com>
2022-01-21 16:59:16 -0500

    remove auto gen users

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,11 +1,11 @@
 <template>
     <div @dblclick="$emit('toggle-read', tweet_id)" 
-         :class="[ gender == 'female' ? 'female' : 'male', tweet.read ? '' : 'unread', 'tweet' ]">
+         :class="[ tweet.gender == 'female' ? 'female' : 'male', tweet.read ? '' : 'unread', 'tweet' ]">
       <div class="left">
         <h3>{{ tweet.text }}</h3>
         <h6><i>{{ formatDate(tweet.date) }}</i></h6>
-        <p>{{ fname }} {{ lname }}</p>
-        <p>{{ location }}</p>
+        <p>{{ tweet.fname }} {{ tweet.lname }}</p>
+        <p>{{ tweet.location }}</p>
       </div>
       <div class="right">
         <i @click="$emit('delete-tweet', tweet_id)"
@@ -36,18 +36,15 @@ export default {
         //     //console.log("deleting tweet that has the id: " + id);
         //     this.$emit('delete-tweet', id)
         // },
-        async getUsers(){
-            const res = await fetch('https://randomuser.me/api');
-            const { results } = await res.json(); 
-            console.log( results );
-            this.fname = results[0].name.first;   
-            this.lname = results[0].name.last;
-            this.location = results[0].location.country;
-            this.gender = results[0].gender;
-        }
-    },
-    mounted(){
-        this.getUsers();
+        // async getUsers(){
+        //     const res = await fetch('https://randomuser.me/api');
+        //     const { results } = await res.json(); 
+        //     console.log( results );
+        //     this.fname = results[0].name.first;   
+        //     this.lname = results[0].name.last;
+        //     this.location = results[0].location.country;
+        //     this.gender = results[0].gender;
+        // }
     },
 }
 </script>
