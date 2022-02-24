52952bd86eb723fee83ecd344d79d0124a1d18bb
jeanwang2dev <jeanharmsen@yahoo.com>
2022-02-24 16:05:39 -0500

    update app title again

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header @toggle-create-tweet="toggleCreateTweet" 
-            title="Tweets Tacker Update" 
+            title="Tweets Tacker Test!!" 
             :showCreateTweet="showCreateTweet" />
     <div v-show="showCreateTweet">
       <CreateTweet @add-tweet="createTweet"/>
